b556a73bee6d9ebfdbe7b24e5c1d1da392592574
sonhaksun <sonhaksun10@gmail.com>
2020-12-03 22:21:18 +0900

    Lastly modified by sonhaksun10

--- src/test/java/com/simplejavacalculator/CalculatorTest.java
+++ src/test/java/com/simplejavacalculator/CalculatorTest.java
@@ -1,7 +1,7 @@
 package com.simplejavacalculator;
 
 import org.junit.jupiter.api.Test;
-
+import static java.lang.Double.NaN;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -24,26 +24,74 @@ class CalculatorTest {
 
         // first opreation always give Nan
         Double res = calc.calculateBi(Calculator.BiOperatorModes.multiply, 2.0);
-        assertEquals(res, Double.NaN);
+        assertEquals(res, NaN);
 
         // second operation
         res = calc.calculateBi(Calculator.BiOperatorModes.multiply, 2.0);
         assertEquals(res, 4.0);
     }
-    /*
-    @Test
-    void calculateBi() {
-    }
 
     @Test
     void calculateEqual() {
+        Calculator calc = new Calculator();
+
+        //one
+        Double res = calc.calculateEqual(0.0);
+        assertNotEquals(0.0, res);
+
+        //two
+        res = calc.calculateEqual(3.0);
+        assertNotEquals(3.0, res);
     }
 
     @Test
     void reset() {
+        Calculator calc = new Calculator();
+
+        Double res = calc.reset();
+        assertEquals(res,NaN);
     }
 
     @Test
     void calculateMono() {
-    }*/
+        Calculator calc = new Calculator();
+
+        Double res = calc.calculateMono(Calculator.MonoOperatorModes.square,0.3);
+        assertEquals(0.09, res);
+
+        res = calc.calculateMono(Calculator.MonoOperatorModes.squareRoot,0.09);
+        assertEquals(0.3, res);
+
+        res = calc.calculateMono(Calculator.MonoOperatorModes.oneDevidedBy,0.25);
+        assertEquals(4.0, res);
+
+        res = calc.calculateMono(Calculator.MonoOperatorModes.cos,75.);
+        assertEquals(Math.cos(75.), res);
+
+        res = calc.calculateMono(Calculator.MonoOperatorModes.sin,75.);
+        assertEquals(Math.sin(75.), res);
+
+        res = calc.calculateMono(Calculator.MonoOperatorModes.tan,75.);
+        assertEquals(Math.tan(75.), res);
+
+        res = calc.calculateMono(Calculator.MonoOperatorModes.tan,270.);
+        assertEquals(NaN, res);
+
+        res = calc.calculateMono(Calculator.MonoOperatorModes.tan,360.);
+        assertEquals(0.0, res);
+
+        res = calc.calculateMono(Calculator.MonoOperatorModes.log,5.2);
+        assertEquals(Math.log10(5.2), res);
+
+        res = calc.calculateMono(Calculator.MonoOperatorModes.rate,79.);
+        assertEquals(0.79, res);
+
+        res = calc.calculateMono(Calculator.MonoOperatorModes.abs,-3.14);
+        assertEquals(3.14, res);
+    }
+    /*
+    @Test
+    void calculateBi() {
+    }
+    */
 }
